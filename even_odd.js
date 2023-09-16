339a2f9fc3cf98e26be1244c525618fe53d63573
Shubhamkr565 <140379407+Shubhamkr565@users.noreply.github.com>
2023-09-16 15:34:21 +0530

    made change
    added comments

--- even_odd.js
+++ even_odd.js
@@ -1,8 +1,8 @@
 console.log("Find the even or odd ");
 if(a%2==0){
-    console.log("Even");
+    console.log("Even");//this is even.
 }
 else
 {
-    console.log("Odd");
+    console.log("Odd");//this is odd
 }
